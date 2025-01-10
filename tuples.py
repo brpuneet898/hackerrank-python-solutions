17b8a99a05ef8f2261a2046922f837e4b094fa3d
Puneet <brpuneet898@gmail.com>
2025-01-10 17:24:38 +0530

    added tuples

---
+++ tuples.py
@@ -0,0 +1,11 @@
+# Task
+# Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n integers. Then compute and print the result of hash(t).
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    lists = [] 
+    for i in integer_list:
+        lists.append(i)
+        
+    print(hash(tuple(lists)))
